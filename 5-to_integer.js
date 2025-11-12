6049951c52d60891f35a8d7ef6c806726e55eef0
Ayuba Yushawu <iyushawu@gmail.com>
2025-11-12 10:39:02 +0000

    Add script that prints My number: <first argument converted in integer>

---
+++ 5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const number = Number(arg);
+
+if (!isNaN(number)) {
+  console.log('My number: ' + parseInt(number));
+} else {
+  console.log('Not a number');
+}
